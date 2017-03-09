57b524dd650f4e91ad312ec420ae8e81b9c9ddc7
Jeemyeong Lee <jeemyeong@gmail.com>
2017-03-09 12:02:56 +0900

    Modify Post Animation

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,9 +4,11 @@ import { Card, Icon } from 'semantic-ui-react'
 const Post = (props) => {
   const userInfo = props.userInfo;
   return (
-    <Card className={`animated zoomInLeft`}>
-      <Card.Content header={userInfo.name!==undefined?userInfo.name:userInfo.email} />
-      <Card.Content description={props.contents} />
+    <Card className={`animated fadeIn`}>
+      <Card.Content
+        header={userInfo.name!==undefined?userInfo.name:userInfo.email}
+        description={props.contents}
+      />
     </Card>
   );
 };
