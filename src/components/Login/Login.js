957cada876417efac8917c8b3d68d19f36087027
Jeemyeong Lee <jeemyeong@gmail.com>
2017-03-09 12:22:16 +0900

    Modify Login view

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,60 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './Login.css'
+import { Button, Form, Label, Icon, Header } from 'semantic-ui-react'
 
 
 class Login extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        email: '',
+        password: ''
+      };
+
+  }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    this.props.onAuthLoginWithEmail(this.email.value, this.pw.value)
+    this.props.onAuthLoginWithEmail(this.state.email, this.state.password)
+    this.setState({email: '', password: ''})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
   }
 
     render() {
         return(
-          <div className="col-sm-6 col-sm-offset-3">
-            <Link to='/register'>Register</Link><br/>
-            <h1> Login </h1>
-            <form onSubmit={this.handleSubmit}>
-              <div className="form-group">
-                <label>Email</label>
-                <input className="form-control" ref={(email) => this.email = email} placeholder="Email"/>
-              </div>
-              <div className="form-group">
-                <label>Password</label>
-                <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
-              </div>
-              <button type="submit" className="btn btn-primary">Login</button>
-            </form>
-            <button onClick={this.props.onAuthLoginWithFacebook}>Facebook Login</button>
+          <div className='Login'>
+
+            <Form onSubmit={this.handleSubmit}>
+
+                <Header as='h2' icon>
+                  <Icon name='sign in' />
+                  Sign In
+                </Header>
+              <Form.Field>
+                <Label pointing="below">Email</Label>
+                <input
+                  value={this.state.email}
+                  onChange={e => this.setState({ email: e.target.value })}
+                  placeholder="Email"
+                />
+              </Form.Field>
+              <Form.Field>
+                <Label pointing="below">Password</Label>
+                <input
+                  type="password"
+                  value={this.state.password}
+                  onChange={e => this.setState({ password: e.target.value })}
+                  placeholder="Password"
+                />
+              </Form.Field>
+              <Button type='submit'>Login</Button>
+              <Button onClick={this.props.onAuthLoginWithFacebook}>Facebook Login</Button>
+              <Link to='/register'><Button>Register</Button></Link>
+            </Form>
           </div>
         );
     }
