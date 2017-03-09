227edf3376521f910a4d0e9744884296ade4a82b
Jeemyeong Lee <jeemyeong@gmail.com>
2017-03-09 11:33:58 +0900

    Add animation for posts

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,7 +4,7 @@ import { Card, Icon } from 'semantic-ui-react'
 const Post = (props) => {
   const userInfo = props.userInfo;
   return (
-    <Card>
+    <Card className={`animated zoomInLeft`}>
       <Card.Content header={userInfo.name!==undefined?userInfo.name:userInfo.email} />
       <Card.Content description={props.contents} />
     </Card>
